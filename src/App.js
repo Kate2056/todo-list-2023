7c9f7117cefa519c3494f9a8640f4f9e2d529706
Kaitlyn Briggs <krbriggs@dmacc.edu>
2023-03-27 20:39:47 -0500

    React.js initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+function App() {  
+  const todoListItems = [
+    'Laundry',
+    'Dishes',
+    'Sweep',
+    'Feed Dog',
+    'Clean Bathroom',
+    'Dust'
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {
+          todoListItems.map((item) => {
+            return <li>{item}</li>
+          }) 
+        }
+      </ul>
+
     </div>
   );
+    
 }
 
 export default App;
